98151b31cbd7e9944bb93cc1e70f9ef942e0c85e
XiongMingcai <xmc000@icloud.com>
2017-12-18 17:00:11 +0800

    配置多个入口出口

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by xiongmingcai on 17/12/18.
+ */
+var path = require('path');
+module.exports = {
+    entry:{
+        one:'./src/one.js',
+        tow:'./src/tow.js'
+    },
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    },
+    module:{},
+    plugins:[],
+    devServer:{}
+};
